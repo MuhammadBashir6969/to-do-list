38ec96268798c2d9fb29b4d6d9e4e6a855532843
MuhammadBashir6969 <muhammadbashir754491830@gmail.com>
2025-07-20 00:24:12 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import ToDoList from './ToDoList';
+import "./App.css";
+import ToDoList from "./ToDoList";
 
 function App() {
   return (
     <div>
-      <ToDoList/>
+      <ToDoList />
     </div>
   );
 }
